6094bf1794e442cef23a7338e424b9ebfdd89c4f
Ionut Damian <damian.ionut23@yahoo.ro>
2021-08-05 13:03:40 +0300

    OOP Examples

---
+++ src/main/java/com/p5/adoptions/OOPExample.java
@@ -0,0 +1,18 @@
+package com.p5.adoptions;
+
+
+import com.p5.adoptions.model.Animal;
+
+/**
+ 1. Abstraction: Model real life scenarios/objects into java classes and objects.
+ 2. Encapsulation: Hide info about classes, allow access through getters and setters
+ 3. Inheritance:
+ 4. Polymorphism:
+ */
+public class OOPExample {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.setName("dog");
+
+    }
+}
---
+++ src/main/java/com/p5/adoptions/model/Animal.java
@@ -0,0 +1,26 @@
+package com.p5.adoptions.model;
+
+public class Animal {
+
+    private String name;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+
+
+}
